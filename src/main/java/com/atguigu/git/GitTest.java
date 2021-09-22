8aebcea28aeb4883615f87b7b97b2edbecd68759
gavin <gavin@qq.com>
2021-09-22 15:32:45 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,15 @@
+package com.atguigu.git;
+
+/**
+ * @author 强哥
+ * @date 2021/9/22 - 15:30
+ */
+public class GitTest {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello Git !!!");
+
+    }
+
+}
